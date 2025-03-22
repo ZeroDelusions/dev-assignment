49bf9d8875db21ea528aaafa68478cb9e91b0a35
ZeroDelusions <dmytro.koso@gmail.com>
2025-03-22 15:42:37 +0100

    Fixed crash due to wrong sql

--- src/main/kotlin/com/zero_delusions/dev_assignment/core/database/builder/InventoryQueryBuilder.kt
+++ src/main/kotlin/com/zero_delusions/dev_assignment/core/database/builder/InventoryQueryBuilder.kt
@@ -35,46 +35,36 @@ class InventoryQueryBuilder {
                 else -> buildQueryForInventory(inventoryType.columnName!!)
             }
 
-            val escapedItemId = "\"$itemId\""
-
-            val query = session.createQuery(hql, UserData::class.java)
-            query.setParameter("itemId", escapedItemId)
+            val query = session.createNativeQuery(hql, UserData::class.java)
 
             return query.resultList
         }
 
         private fun buildInventoryCondition(columnName: String): String {
-            // We check if there exists at least some item in a specified inventory
-            // that meets the requirements, by:
-            // 1. Transforming JSON string into table
-            // 2. For each item in that table (array), we perform action
-            // 3. We put a whole object as item_data column
-            // 4. Define the whole table as `items`
-            // 5. For elements that aren't `null`, we perform min/max checks
-
-            // (I hope I am correct on that)
             return """
                 EXISTS (
-                    SELECT 1 from JSON_TABLE(
-                        u.$columnName->'$.values',
-                        '$[*]' COLUMNS (item_data VARCHAR(255) PATH '$')
-                    ) as items
+                    SELECT 1 FROM JSON_TABLE(
+                        JSON_EXTRACT(u.$columnName, '$.values'), '$[*]'
+                        COLUMNS (
+                            item_data VARCHAR(512) PATH '$'
+                        )
+                    ) AS items
                     WHERE items.item_data IS NOT NULL 
-                    AND JSON_EXTRACT(items.item_data, '$.id') = :itemId
-                    ${if (minCount > 0) "AND JSON_EXTRACT(items.item_data, '$.count') >= $minCount " else ""}            
-                    ${if (maxCount < Int.MAX_VALUE) "AND JSON_EXTRACT(items.item_data, '$.count') =< $maxCount " else ""}            
+                      AND JSON_EXTRACT(items.item_data, '$.id') = '${this.itemId}'
+                      ${if (minCount > 0) "AND JSON_EXTRACT(JSON_UNQUOTE(items.item_data), '$.count') >= $minCount " else ""}
+                      ${if (maxCount < Int.MAX_VALUE) "AND JSON_EXTRACT(JSON_UNQUOTE(items.item_data), '$.count') <= $maxCount " else ""}
                 )
             """.trimIndent()
         }
 
         private fun buildQueryForAllInventories(): String {
-            return "FROM UserData u WHERE " + InventoryType.entries
+            return "SELECT * FROM user_data u WHERE " + InventoryType.entries
                 .filter { it != InventoryType.ALL }
                 .joinToString(" OR ") { buildInventoryCondition(it.columnName!!) }
         }
 
         private fun buildQueryForInventory(columnName: String): String {
-            return "FROM UserData u WHERE ${buildInventoryCondition(columnName)}"
+            return "SELECT * FROM user_data u WHERE ${buildInventoryCondition(columnName)}"
         }
     }
 }
