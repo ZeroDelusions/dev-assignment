9c9ce699f6f872fae64834360b0958366496aae2
ZeroDelusions <dmytro.koso@gmail.com>
2025-03-22 11:07:09 +0100

    Added HibernateUtils

---
+++ src/main/kotlin/com/zero_delusions/dev_assignment/core/HibernateUtils.kt
@@ -0,0 +1,20 @@
+package com.zero_delusions.dev_assignment.core
+
+import org.hibernate.SessionFactory
+import org.hibernate.cfg.Configuration
+
+object HibernateUtils {
+    @JvmField
+    val sessionFactory = buildSessionFactory()
+
+    private fun buildSessionFactory(): SessionFactory {
+        val configuration = Configuration()
+
+        val username = System.getenv(System.getenv("DB_USERNAME"))
+        val password = System.getenv(System.getenv("DB_PASSWORD"))
+        configuration.setProperty("hibernate.connection.username", username)
+        configuration.setProperty("hibernate.connection.password", password)
+
+        return configuration.buildSessionFactory()
+    }
+}
